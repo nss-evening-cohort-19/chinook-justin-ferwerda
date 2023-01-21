98285553c6f36709f710aa1f807821fb0275b367
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-01-21 13:41:47 -0600

    invoice_totals

--- sales_agent_invoices.sql
+++ sales_agent_invoices.sql
@@ -1,4 +1,4 @@
-SELECT Employee.FirstName, Employee.LastName
+SELECT Employee.FirstName, Employee.LastName, Invoice.InvoiceId
 FROM Invoice 
-INNER JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+INNER JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
 INNER JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
