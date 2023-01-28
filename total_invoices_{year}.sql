764c396752f25acdacdae23d86e49c12a2bcf8b1
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-01-28 12:09:20 -0600

    total_sales_year

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*) as 'Invoices for 2009 or 2011'
+SELECT COUNT(InvoiceId) as 'Invoices for 2009 or 2011'
 FROM Invoice
 WHERE strftime('%Y', InvoiceDate) == '2009' or strftime('%Y', InvoiceDate) == '2011'
